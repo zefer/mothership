d88a65de7710b0be82a3a6d27781fcdc74b4bcf0
Joe Roberts <joe@zefer.co.uk>
2014-09-16 21:02:19 +0100

    Retry watcher connection on start-up

--- mpd.go
+++ mpd.go
@@ -1,24 +1,46 @@
 package main
 
 import (
+	"time"
+
 	"github.com/golang/glog"
 	"github.com/zefer/gompd/mpd"
 )
 
+const retryDur time.Duration = time.Second * 3
+
 type watchConn struct {
 	watcher *mpd.Watcher
+	addr    string
 }
 
 func newWatchConn(addr string) (*watchConn, error) {
-	w, err := mpd.NewWatcher("tcp", addr, "")
-	if err != nil {
-		return nil, err
-	}
-	conn := &watchConn{w}
+	conn := &watchConn{addr: addr}
+	conn.retryConnect()
 	go conn.errorLoop()
 	go conn.eventLoop()
-
 	return conn, nil
+}
+
+func (w *watchConn) connect() error {
+	watcher, err := mpd.NewWatcher("tcp", w.addr, "")
+	if err != nil {
+		return err
+	}
+	w.watcher = watcher
+	return nil
+}
+
+func (w *watchConn) retryConnect() {
+	for {
+		err := w.connect()
+		if err == nil {
+			glog.Infof("Watcher: connected to %s", w.addr)
+			return
+		}
+		glog.Errorf("Watcher: connect failed. Waiting then retrying. %v", err)
+		time.Sleep(retryDur)
+	}
 }
 
 func (w *watchConn) Close() {
