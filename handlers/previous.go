fe75efb5144538072fb6ea5219656677a9606074
Joe Roberts <joe@zefer.co.uk>
2015-03-17 16:14:44 +0000

    Move controls handlers into separate files for readability

---
+++ handlers/previous.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/golang/glog"
+)
+
+type Previouser interface {
+	Previous() error
+}
+
+func PreviousHandler(c Previouser) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := c.Previous()
+		if err != nil {
+			glog.Errorln(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+}
