ab4a4f6a51aa425f257b6d5311639e3e78604419
Joe Roberts <joe@zefer.co.uk>
2015-03-17 15:45:42 +0000

    Refactor & test /pause handler

--- handlers/controls.go
+++ handlers/controls.go
@@ -55,9 +55,13 @@ func PlayHandler(c Player) http.Handler {
 	})
 }
 
-func PauseHandler(c *mpd.Client) http.Handler {
+type Pauser interface {
+	Pause(pause bool) error
+}
+
+func PauseHandler(c Pauser) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := c.C.Pause(true)
+		err := c.Pause(true)
 		if err != nil {
 			glog.Errorln(err)
 			w.WriteHeader(http.StatusInternalServerError)
