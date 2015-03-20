f88a444f282a31d57f2687c8d087cec3b7d093f2
Joe Roberts <joe@zefer.co.uk>
2015-03-20 11:15:32 +0000

    Additional tests for /playlist listing

--- handlers/playlist_test.go
+++ handlers/playlist_test.go
@@ -1,6 +1,8 @@
 package handlers_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 
@@ -17,7 +19,22 @@ func (c mockPlClient) Status() (mpd.Attrs, error) {
 }
 
 func (c mockPlClient) PlaylistInfo(start, end int) ([]mpd.Attrs, error) {
-	return make([]mpd.Attrs, 0), nil
+	pls := []mpd.Attrs{
+		{
+			"file":          "Led Zeppelin - Houses Of The Holy/08 - Led Zeppelin - The Ocean.mp3",
+			"Artist":        "Led Zeppelin",
+			"Title":         "The Ocean",
+			"Album":         "Houses of the Holy",
+			"Last-Modified": "2010-12-09T21:32:02Z",
+			"Pos":           "0",
+		},
+		{
+			"file":          "Johnny Cash – Unchained/Johnny Cash – Sea Of Heartbreak.mp3",
+			"Last-Modified": "2011-10-09T11:45:11Z",
+			"Pos":           "1",
+		},
+	}
+	return pls, nil
 }
 
 func (c mockPlClient) Clear() error {
@@ -60,6 +77,44 @@ var _ = Describe("PlayListHandler", func() {
 				Expect(w.Code).To(Equal(http.StatusMethodNotAllowed))
 				Expect(w.Body.String()).To(Equal(""))
 			}
+		})
+	})
+
+	Context("with a GET request", func() {
+		var client *mockPlClient
+
+		BeforeEach(func() {
+			client = &mockPlClient{}
+			handler = handlers.PlayListHandler(client)
+		})
+
+		It("responds with 200 OK", func() {
+			req, _ := http.NewRequest("GET", "/playlist", nil)
+			handler.ServeHTTP(w, req)
+			Expect(w.Code).To(Equal(http.StatusOK))
+		})
+
+		It("responds with the JSON content-type", func() {
+			req, _ := http.NewRequest("GET", "/playlist", nil)
+			handler.ServeHTTP(w, req)
+			Expect(w.HeaderMap["Content-Type"][0]).To(Equal("application/json"))
+		})
+
+		It("responds with a JSON array of playlist items", func() {
+			req, _ := http.NewRequest("GET", "/playlist", nil)
+			handler.ServeHTTP(w, req)
+			var pls []map[string]interface{}
+			if err := json.NewDecoder(w.Body).Decode(&pls); err != nil {
+				Fail(fmt.Sprintf("Could not parse JSON %v", err))
+			}
+			// Item 1 has artist & track parts, so we expect "artist - track".
+			Expect(len(pls[0])).To(Equal(2))
+			Expect(pls[0]["pos"]).To(BeEquivalentTo(1))
+			Expect(pls[0]["name"]).To(Equal("Led Zeppelin - The Ocean"))
+			// Item 2 doesn't have artist & track parts, so we expect "file.mp3".
+			Expect(len(pls[1])).To(Equal(2))
+			Expect(pls[1]["pos"]).To(BeEquivalentTo(2))
+			Expect(pls[1]["name"]).To(Equal("Johnny Cash – Sea Of Heartbreak.mp3"))
 		})
 	})
 })
